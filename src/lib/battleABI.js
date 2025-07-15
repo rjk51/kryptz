9a07a16864ed92f0e80bc0a7326ebcb7e9e0e081
bhumisengar2004 <sengarbhumika29@gmail.com>
2025-07-15 16:55:32 +0530

    feat: add matchmaking UI and logic

--- src/lib/battleABI.js
+++ src/lib/battleABI.js
@@ -2,4 +2,4 @@
 import battleGameArtifact from "./BattleGameABI.json";
 
 export const battleGameABI = battleGameArtifact.abi;
-export const battleGameAddress = "0x7bf59FD7832FD14249a452Bc5969201A74934d8d";
+export const battleGameAddress = "0x28449397738593644FaDD18Cf4334ECf0B466296";
